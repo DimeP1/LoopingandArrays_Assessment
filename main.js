5e2f03ed93c3d5e2a534b439c9e22e4c4978fd35
DimeP1 <71714575+DimeP1@users.noreply.github.com>
2020-10-26 17:43:12 -0400

    add

---
+++ main.js
@@ -0,0 +1,20 @@
+for (let i = 1000; i >= 0; i-=2) {
+    console.log(i);
+}
+
+for (let x = 0; x <= 10000; x++) {
+    console.log(x);
+} if(x = 2500) {
+alert('A quarter of the way there!');
+}if (x = 5000) {
+     alert('Halfway there!');
+}if (x = 10000) {
+ alert('The loop is done!');
+}
+
+const favoriteTvShows = ['How To Get Away With Murder', 'Game of Thrones', 'Blackish', 'Grownish', 'Good Trouble', 'The Bold Type'];
+
+
+for (let index = 0; index < favoriteTvShows.length; index++) {
+    console.log("My #1 favorite tv show is " +favoriteTvShows[0] +" My #2 favorite tv show is " +favoriteTvShows[1]  +" My #3 favorite tv show is " +favoriteTvShows[2] +" My #4 favorite tv show is " +favoriteTvShows[3]   + " My #5 favorite tv show is " +favoriteTvShows[4]);
+}
